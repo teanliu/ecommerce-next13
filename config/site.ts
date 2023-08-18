1dfec7b94af572baaeee1d1f6bc6a7a86e0edffe
teanliu <teanliuu@gmail.com>
2023-08-19 06:35:49 +0800

    first commit

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "H&Buy",
+  description: "Expertly designed goods for the workplace, home, and travel.",
+  footer: [
+    { name: "Home", href: "/" },
+    { name: "About", href: "#" },
+    { name: "Terms & Conditions", href: "#" },
+    { name: "Shipping & Return Policy", href: "#" },
+    { name: "Privacy Policy", href: "#" },
+    { name: "FAQ", href: "#" },
+  ],
+}
