f33ef25bcbcfdeb4eedfc22232a83f1b54fba49b
teanliu <teanliuu@gmail.com>
2023-08-22 05:28:31 +0800

    feat: update page.tsx && product-filters.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -18,18 +18,29 @@ import { ProductSort } from "@/components/product-sort";
 interface Props {
   searchParams: {
     date?: string,
-    price?: string
+    price?: string,
+    color?: string,
+    category?: string,
+    size?: string
   }
 }
 
 export default async function Page({ searchParams }: Props) {
-  const { date = "desc", price } = searchParams
+  const { date = "desc", price, color, category, size } = searchParams
   const priceOrder = price? `| order(price ${price})` : ""
   const dateOrder = date? `| order(_createdAt ${date})` : ""
   const order = `${priceOrder}${dateOrder}`
+
+  const productFilter = `_type == "product"`
+  const colorFilter = color ? `&& "${color}" in colors` : ""
+  const categoryFilter = category ? `&& "${category}" in categories` : ""
+  const sizeFilter = size ? `&& "${size}" in sizes` : ""
+
+  const filter = `*[${productFilter}${colorFilter}${categoryFilter}${sizeFilter}]`
+
   const products = await client.fetch<
     SanityProduct[]
-  >(groq`*[_type == "product"] ${order} {
+  >(groq`${filter} ${order} {
     _id,
     _createdAt,
     name,
@@ -40,7 +51,6 @@ export default async function Page({ searchParams }: Props) {
     description,
     "slug": slug.current
   }`)
-  // console.log(products)
   return (
     <div>
       <div className="px-4 pt-20 text-center">
