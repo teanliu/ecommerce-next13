f3ec9d3cca6feb0942b661cb421a171c205fe12d
teanliu <teanliuu@gmail.com>
2023-08-23 13:33:47 +0800

    feat: update product-gallery.tsx

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -35,7 +35,7 @@ export default async function Page({ params }: Props) {
         {/* Product */}
         <div className="pb-20 lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-12">
           {/* Product gallery */}
-          {/* <ProductGallery product={product} /> */}
+          <ProductGallery product={product} />
           {/* Product info */}
           <ProductInfo product={product} />
         </div>
