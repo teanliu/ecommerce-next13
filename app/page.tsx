1df3f20495ea5a7e10c56c9f7adbfed9c62c580f
teanliu <teanliuu@gmail.com>
2023-08-23 13:21:37 +0800

    feat: update Product-info.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -21,12 +21,13 @@ interface Props {
     price?: string,
     color?: string,
     category?: string,
-    size?: string
+    size?: string,
+    search?: string
   }
 }
 
 export default async function Page({ searchParams }: Props) {
-  const { date = "desc", price, color, category, size } = searchParams
+  const { date = "desc", price, color, category, size, search } = searchParams
   const priceOrder = price? `| order(price ${price})` : ""
   const dateOrder = date? `| order(_createdAt ${date})` : ""
   const order = `${priceOrder}${dateOrder}`
@@ -35,8 +36,9 @@ export default async function Page({ searchParams }: Props) {
   const colorFilter = color ? `&& "${color}" in colors` : ""
   const categoryFilter = category ? `&& "${category}" in categories` : ""
   const sizeFilter = size ? `&& "${size}" in sizes` : ""
+  const searchFilter = search ? `&& name match "${search}"` : ""
 
-  const filter = `*[${productFilter}${colorFilter}${categoryFilter}${sizeFilter}]`
+  const filter = `*[${productFilter}${colorFilter}${categoryFilter}${sizeFilter}${searchFilter}]`
 
   const products = await client.fetch<
     SanityProduct[]
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -9,9 +9,22 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
+import React from "react"
 
 export function SiteHeader() {
   const pathname = usePathname()
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const { cartCount } = useShoppingCart()
+  const defaultSearchQuery = searchParams.get("search") ?? ""
+
+  function onSubmit (event: React.SyntheticEvent<HTMLFormElement>) {
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+    const searchQuery = formData.get("search")
+    router.replace(`/?search=${searchQuery}`)
+
+  }
 
   if (pathname.startsWith("/studio")) return null
 
@@ -19,7 +32,7 @@ export function SiteHeader() {
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="mx-auto flex h-16 max-w-6xl items-center justify-between space-x-4 px-6 sm:space-x-0">
         <MainNav />
-        <form className="hidden items-center lg:inline-flex">
+        <form onSubmit={onSubmit} className="hidden items-center lg:inline-flex">
           <Input
             id="search"
             name="search"
@@ -27,13 +40,14 @@ export function SiteHeader() {
             autoComplete="off"
             placeholder="Search products..."
             className="h-9 lg:w-[300px]"
+            defaultValue={defaultSearchQuery}
           />
         </form>
         <div className="flex items-center space-x-1">
           <Link href="/cart">
             <Button size="sm" variant="ghost">
               <ShoppingBag className="h-5 w-5" />
-              <span className="ml-2 text-sm font-bold">0</span>
+              <span className="ml-2 text-sm font-bold">{cartCount}</span>
               <span className="sr-only">Cart</span>
             </Button>
           </Link>
